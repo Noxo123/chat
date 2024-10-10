3b6b10188f8f16ef6cd738a55daf6711c2bf735f
Noxo123 <138624794+Noxo123@users.noreply.github.com>
2024-10-10 21:01:18 +0200

    Add files via upload

---
+++ scripts/main/config.js
@@ -0,0 +1,23 @@
+// ISI DENGAN PROJECT FIREBASE MILIK TEMEN-TEMEN SENDIRI
+const firebaseConfig = {
+  apiKey: "AIzaSyCstbUl57RLaz12wf-vcrAqTJiaP0poDOE",
+  authDomain: "site-428214.firebaseapp.com",
+  projectId: "site-428214",
+  storageBucket: "site-428214.appspot.com",
+  messagingSenderId: "550512586578",
+  appId: "1:550512586578:web:ede6fc9afdebe3b59e948b",
+  measurementId: "G-N4BDSWG8T5"
+};
+
+// BIAR GA KELAMAAN PAS MAU PANGGIL NANTI
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const rdb = firebase.database();
+const stg = firebase.storage();
+
+/*
+    SUBSCRIBE: DEVANKA 761 
+    https://www.youtube.com/c/RG761
+
+    IG: " @dvnkz_ "
+*/
